99a6a6982311191278efcc53b3a34097591a705f
Erik Szabó <erik1szabo1@gmail.com>
2020-06-22 16:46:30 +0200

    new logs page

--- src/frontend/components/log-page.js
+++ src/frontend/components/log-page.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'https://unpkg.com/lit-element?module';
+import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 import { logItem, logPage } from './styles.js';
 
 /**
